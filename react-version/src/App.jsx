84ce891026c8a5509417044b748a1c70ea826c56
Alec Faustino <alecfaustino@outlook.com>
2025-04-15 20:02:11 -0600

    import sliced HTML into react components

--- react-version/src/App.jsx
+++ react-version/src/App.jsx
@@ -9,8 +9,10 @@ function App() {
     <div>
       <Navigation />
       <Profile />
-      <TweetList />
-      <TweetForm />
+      <main className="container">
+        <TweetForm />
+        <TweetList />
+      </main>
     </div>
   )
 }
